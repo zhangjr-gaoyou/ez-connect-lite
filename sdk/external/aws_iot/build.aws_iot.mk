ba9f46a5608811fe2cfc9660967c22d804435044
Nikita Nerkar <nikitan@marvell.com>
2016-08-10 19:45:33 +0530

    Upgrade to AWS IoT Agent 2.1

--- sdk/external/aws_iot/build.aws_iot.mk
+++ sdk/external/aws_iot/build.aws_iot.mk
@@ -3,20 +3,8 @@
 
 libs-y += libaws_iot
 
-libaws_iot-objs-y := aws_mqtt_embedded_client_lib/MQTTPacket/src/MQTTSerializePublish.c \
-	aws_mqtt_embedded_client_lib/MQTTPacket/src/MQTTUnsubscribeClient.c \
-	aws_mqtt_embedded_client_lib/MQTTPacket/src/MQTTConnectClient.c \
-	aws_mqtt_embedded_client_lib/MQTTPacket/src/MQTTPacket.c \
-	aws_mqtt_embedded_client_lib/MQTTPacket/src/MQTTSubscribeClient.c \
-	aws_mqtt_embedded_client_lib/MQTTPacket/src/MQTTDeserializePublish.c \
-	aws_mqtt_embedded_client_lib/MQTTClient-C/src/MQTTClient.c \
-	aws_iot_src/protocol/mqtt/aws_iot_embedded_client_wrapper/aws_iot_mqtt_embedded_client_wrapper.c \
-	aws_iot_src/utils/aws_iot_json_utils.c \
-	aws_iot_src/protocol/mqtt/aws_iot_embedded_client_wrapper/platform_wmsdk/network_interface.c \
-	aws_iot_src/shadow/aws_iot_shadow_json.c \
-	aws_iot_src/shadow/aws_iot_shadow_actions.c \
-	aws_iot_src/shadow/aws_iot_shadow.c \
-	aws_iot_src/shadow/aws_iot_shadow_records.c \
-	aws_iot_src/protocol/mqtt/aws_iot_embedded_client_wrapper/platform_wmsdk/timer.c \
+aws_iot_src_dir := $(d)/src
 
-libaws_iot-cflags-y := -I $(d)/aws_iot_src/protocol/mqtt/aws_iot_embedded_client_wrapper -I $(d)/aws_iot_src/shadow -I $(d)aws_iot_src/protocol/mqtt -I $(d)/aws_iot_src/utils -I $(d)/aws_mqtt_embedded_client_lib/MQTTPacket/src -I $(d)/aws_mqtt_embedded_client_lib/MQTTClient-C/src
+libaws_iot-objs-y := $(wildcard $(aws_iot_src_dir)/*.c) $(d)/platform/wmsdk/timer/timer.c $(d)/platform/wmsdk/network/network_interface.c
+
+libaws_iot-objs-y := $(libaws_iot-objs-y:$(d)/%=%)
